8912d98909e60468a09bb3fdfa2d22856e734986
azad-nstu <108252939+azad-nstu@users.noreply.github.com>
2022-12-04 22:20:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,45 +6,25 @@ from matplotlib.ticker import StrMethodFormatter
 import streamlit as st
 from PIL import Image
 
-
 # Load  model a 
-model = joblib.load(open("log_reg_model.sav","rb"))
+model = joblib.load(open("decision_tree_model.sav","rb"))
 
 def data_preprocessor(df):
     """this function preprocess the user input
         return type: pandas dataframe
     """
-    df.wine_type = df.wine_type.map({'white':0, 'red':1})
+    df.Smoking = df.Smoking.map({'Yes':1, 'No':0})
+    df.Stroke = df.Stroke.map({'Yes':1, 'No':0})
+    df.DiffWalking = df.DiffWalking.map({'Yes':1, 'No':0})
+    df.Sex = df.Sex.map({'Yes':1, 'No':0})
+    df.Diabetic = df.Diabetic.map({'Yes':1, 'No':0})
+    df.PhysicalActivity = df.PhysicalActivity.map({'Yes':1, 'No':0})
+    df.KidneyDisease = df.KidneyDisease.map({'Yes':1, 'No':0})
+    df.SkinCancer = df.SkinCancer.map({'Yes':1, 'No':0})
+    df.AgeCategory = df.SkinCancer.map({'18-24':0,'25-29':1,'30-34':2,'35-39':3,'40-44':4,'45-49':5,'50-54':6,'55-59':7,'60-64':8,'65-69':9,'70-74':10,'75-79':11,'80 or older':12})
+    df.GenHealth = df.GenHealth.map({'Excellent':0,'Very good':1,'Good':2,'Fair':3,'Poor':4})
+   
     return df
-
-def visualize_confidence_level(prediction_proba):
-    """
-    this function uses matplotlib to create inference bar chart rendered with streamlit in real-time 
-    return type : matplotlib bar chart  
-    """
-    data = (prediction_proba[0]*100).round(2)
-    grad_percentage = pd.DataFrame(data = data,columns = ['Percentage'],index = ['Low','Ave','High'])
-    ax = grad_percentage.plot(kind='barh', figsize=(7, 4), color='#722f37', zorder=10, width=0.5)
-    ax.legend().set_visible(False)
-    ax.set_xlim(xmin=0, xmax=100)
-    
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
-    ax.spines['left'].set_visible(True)
-    ax.spines['bottom'].set_visible(True)
-
-    ax.tick_params(axis="both", which="both", bottom="off", top="off", labelbottom="on", left="off", right="off", labelleft="on")
-    
-    vals = ax.get_xticks()
-    for tick in vals:
-        ax.axvline(x=tick, linestyle='dashed', alpha=0.4, color='#eeeeee', zorder=1)
-
-    ax.set_xlabel(" Percentage(%) Confidence Level", labelpad=2, weight='bold', size=12)
-    ax.set_ylabel("Wine Quality", labelpad=10, weight='bold', size=12)
-    ax.set_title('Prediction Confidence Level ', fontdict=None, loc='center', pad=None, weight='bold')
-
-    st.pyplot()
-    return
 
 st.write("""
 # Heart Disease Prediction using ML Model 
@@ -57,38 +37,38 @@ st.image(image, caption='wine company',use_column_width=True)
 
 st.sidebar.header('User Input Parameters') #user input parameter collection with streamlit side bar
 
-
 def get_user_input():
     """
     this function is used to get user input using sidebar slider and selectbox 
     return type : pandas dataframe
 
     """
-    wine_type = st.sidebar.selectbox("Select Wine type",("white", "red"))
-    fixed_acidity = st.sidebar.slider('fixed acidity', 3.8, 15.9, 7.0)
-    volatile_acidity = st.sidebar.slider('volatile acidity', 0.08, 1.58, 0.4)
-    citric_acid  = st.sidebar.slider('citric acid', 0.0, 1.66, 0.3)
-    residual_sugar  = st.sidebar.slider('residual_sugar', 0.6, 65.8, 10.4)
-    chlorides  = st.sidebar.slider('chlorides', 0.009, 0.611, 0.211)
-    free_sulfur_dioxide = st.sidebar.slider('free sulfur dioxide', 1, 289, 200)
-    total_sulfur_dioxide = st.sidebar.slider('total sulfur dioxide', 6, 440, 150)
-    density = st.sidebar.slider('density', 0.98, 1.03, 1.0)
-    pH = st.sidebar.slider('pH', 2.72, 4.01, 3.0)
-    sulphates = st.sidebar.slider('sulphates', 0.22, 2.0, 1.0)
-    alcohol = st.sidebar.slider('alcohol', 8.0, 14.9, 13.4)
+    Smoking = st.sidebar.selectbox("Do you smoke?",("Yes", "No"))
+    Stroke = st.sidebar.selectbox("Did you stroke ever?",("Yes", "No"))
+    DiffWalking = st.sidebar.selectbox("Do you have difficulty in walking?",("Yes", "No"))
+    Sex = st.sidebar.selectbox("Gender?",("Yes", "No"))
+    Diabetic = st.sidebar.selectbox("Are you a diabetic patient?",("Yes", "No"))
+    PhysicalActivity = st.sidebar.selectbox("Do you exercise?",("Yes", "No"))
+    KidneyDisease = st.sidebar.selectbox("Do you have kidney disease?",("Yes", "No"))
+    SkinCancer = st.sidebar.selectbox("Do you have skin cancer?",("Yes", "No"))
+    AgeCategory = st.sidebar.selectbox("Whats your age range?",("18-24", "25-29","30-34","35-39","40-44","45-49","50-54","55-59","60-64","65-69","70-74","75-79","80 or older"))
+    GenHealth = st.sidebar.selectbox("What's your general health condition?",("Excellent", "Very good","Good","Fair","Poor"))
     
-    features = {'wine_type': wine_type,
-            'fixed_acidity': fixed_acidity,
-            'volatile_acidity': volatile_acidity,
-            'citric_acid': citric_acid,
-            'residual_sugar': residual_sugar,
-            'chlorides': chlorides,
-            'free_sulfur_dioxide': free_sulfur_dioxide,
-            'total_sulfur_dioxide': total_sulfur_dioxide,
-            'density': density,
-            'pH': pH,
-            'sulphates': sulphates,
-            'alcohol': alcohol
+    PhysicalHealth = st.sidebar.slider('PhysicalHealth', 0.0, 30, 1)
+    BMI = st.sidebar.slider('BMI', 12.0, 94.85, 0.05)
+     
+    features = {'Smoking': Smoking,
+            'Stroke': Stroke,
+            'DiffWalking': DiffWalking,
+            'Sex': Sex,
+            'Diabetic': Diabetic,
+            'PhysicalActivity': PhysicalActivity,
+            'KidneyDisease': KidneyDisease,
+            'SkinCancer': SkinCancer,
+            'AgeCategory': AgeCategory,
+            'GenHealth': GenHealth,
+            'PhysicalHealth': PhysicalHealth,
+            'BMI': BMI
             }
     data = pd.DataFrame(features,index=[0])
 
@@ -101,6 +81,15 @@ st.subheader('User Input parameters')
 st.write(user_input_df)
 
 prediction = model.predict(processed_user_input)
-prediction_proba = model.predict_proba(processed_user_input)
 
-visualize_confidence_level(prediction_proba)
+# creating a button for Prediction 
+heart_diagnosis = ''
+if st.button('Heart Disease Diagnosis'):       
+    if (prediction[0] == 1):
+        heart_diagnosis = 'The person is having heart disease'
+    else:
+        heart_diagnosis = 'The person does not having heart disease'
+        
+st.success(heart_diagnosis)
+
+
