a39279cebdc6e016ec934f6f41401f7676a77b8a
azad-nstu <108252939+azad-nstu@users.noreply.github.com>
2022-12-06 02:39:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 from PIL import Image
 
 # Load  model a 
-model = joblib.load(open("decision_tree_model.joblib","rb"))
+model = joblib.load(open("decision_tree_model-v1.joblib","rb"))
 
 def data_preprocessor(df):
     """this function preprocess the user input
